240d69c89cee7d476f3b8be41877fb2c78e29e21
ASHOKA M <ashokashokm2001@gmail.com>
2026-02-17 20:28:20 +0530

    day 7 and day 8 class

---
+++ Day-8 Class/script.js
@@ -0,0 +1,52 @@
+// variable_a=10;
+
+// a="java"
+// class Demo{
+//     display(){
+//         console.log("This display method  present in Demo Class");
+        
+//     }
+// }
+
+// function addition(){
+//     console.log(10+20);
+    
+// }
+
+
+// fun=()=>{}
+
+
+
+
+   /* a=10
+    b="10"
+
+    console.log(a===b);
+
+    */
+
+    
+
+
+    a=10;
+    str="java"
+    str1='javascript'
+    str2 =`n`
+    b=true;
+
+    c=Symbol("hello")
+
+    m=null;
+
+    n=267986573695876879857683254336n
+    console.log(typeof a);
+    console.log(typeof str);
+    console.log(typeof str1);
+    console.log(typeof str2);
+    console.log(typeof b);
+    console.log(typeof c);
+    console.log(typeof n);
+    console.log(typeof m);
+
+    
